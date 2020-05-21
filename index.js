1328e1cb7afd839eb34ebd8637100fe0be5290cd
tamdilip <tamdilip@gmail.com>
2020-05-21 17:28:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+async function runScript() {
+    const core = require('@actions/core');
+    const github = require('@actions/github');
+    const path = require('path');
+    const url = require('url');
+    const fs = require('fs');
+
+    const ev = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+    const prNum = ev.pull_request.number;
+    const changedFiles = await github.pulls.listFiles({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: prNum
+    });
+
+    const filename = changedFiles.data[0].filename;
+    console.log(filename);
+
+    const reportPath = path.resolve('eslint_report.json');
+    console.log(reportPath);
+
+    const reportFile = await fs.readFileSync(reportPath, 'utf-8');
+    console.log(reportFile);
+
+    const reportContents = JSON.parse(reportFile);
+    console.log(reportContents);
+
+
+    const url_parts = url.parse(changedFiles.data[0].contents_url, true);
+    const commitId = url_parts.query.ref;
+
+    github.pulls.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: prNum,
+        body: reportContents.filter(es => es.errorCount > 0)[0].messages[0].message,
+        commit_id: commitId,
+        path: filename,
+        line: reportContents.filter(es => es.errorCount > 0)[0].messages[0].line
+    });
+
+}
+
+runScript();
