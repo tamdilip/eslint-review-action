8e92a427a476bb32a38794a2d29e5b187b632fff
tamdilip <tamdilip@gmail.com>
2020-05-21 18:34:31 +0530

    refactored

--- dist/index.js
+++ dist/index.js
@@ -24948,43 +24948,34 @@ async function runScript() {
     const url = __webpack_require__(835);
     const fs = __webpack_require__(747);
 
-    const ev = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
-    const prNum = ev.pull_request.number;
-    console.log(prNum);
-
     const repoToken = core.getInput('repo-token');
-
     const octokit = new github.GitHub(repoToken);
     const context = github.context;
+    const { repo: { owner, repo } } = context;
+
+    const eventPath = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+    const pull_number = eventPath.pull_request.number;
 
     const changedFiles = await octokit.pulls.listFiles({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        pull_number: prNum
+        owner,
+        repo,
+        pull_number
     });
-
     const filename = changedFiles.data[0].filename;
-    console.log(filename);
 
     const reportPath = path.resolve('eslint_report.json');
-    console.log(reportPath);
-
-    const reportFile = await fs.readFileSync(reportPath, 'utf-8');
-    console.log(reportFile);
-
+    const reportFile = fs.readFileSync(reportPath, 'utf-8')
     const reportContents = JSON.parse(reportFile);
-    console.log(reportContents);
-
 
     const url_parts = url.parse(changedFiles.data[0].contents_url, true);
-    const commitId = url_parts.query.ref;
+    const commit_id = url_parts.query.ref;
 
     octokit.pulls.createComment({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        pull_number: prNum,
+        owner,
+        repo,
+        pull_number,
         body: reportContents.filter(es => es.errorCount > 0)[0].messages[0].message,
-        commit_id: commitId,
+        commit_id,
         path: filename,
         line: reportContents.filter(es => es.errorCount > 0)[0].messages[0].line
     });
