086b39a0535def6e40527b4fe642e887bc958961
Dilipan Somasundaram <tamdilip@gmail.com>
2020-05-26 00:28:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function runScript() {
         let fixed = subArr[0].includes("✔️"),
             lineUrl = fixed ? subArr[0].replace("✔️", "").replace(/\s+/g, ' ').trim() : subArr[0].replace("⛔", "").replace(/\s+/g, ' ').trim(),
             message = subArr[1].replace("**", "").replace("---", "").replace(/\s+/g, ' ').trim(),
-            repoRemovedPath = lineUrl.replace("https://github.com/${owner}/${repo}/blob/", ""),
+            repoRemovedPath = lineUrl.replace(`https://github.com/${owner}/${repo}/blob/`, ""),
             path = repoRemovedPath.substring(repoRemovedPath.indexOf("/") + 1, repoRemovedPath.indexOf("#")),
             line = repoRemovedPath.substring(repoRemovedPath.lastIndexOf("#") + 2, repoRemovedPath.length),
             sha = repoRemovedPath.substring(0, repoRemovedPath.indexOf("/"));
